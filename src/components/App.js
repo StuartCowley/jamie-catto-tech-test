9356769b26a1976efc3705cd45c38c7d34dec501
Jamie Catto <jamietcatto@gmail.com>
2023-02-03 15:34:53 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <p>Hello world!</p>
+    </div>
+  );
+}
+
+export default App;
