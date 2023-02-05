d2863891ad3789c2a46f1cb179e720f4adbffb3e
Jamie Catto <jamietcatto@gmail.com>
2023-02-05 17:33:29 +0100

    Added empty stateless component for Search Results

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import Search from './Search';
+import SearchResults from './SearchResults';
 import '../styles/app.css';
 
 const App = () => {
   const [searchResults, setSearchResults] = useState();
-  
+
   return (
     <div className="app">
       <img src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" 
       alt="NASA Logo" className="nasa-logo"></img>
       <Search
       setSearchResults={setSearchResults} />
+      <SearchResults />
     </div>
   );
 }
---
+++ src/components/SearchResults.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/searchResults.css';
+
+const SearchResults = () => {
+    return (
+        <>
+        <p>Search Results</p>
+        </>
+    )
+}
+
+export default SearchResults;
