2b026443fa674d8758885311eb813327bafd543b
Jamie Catto <jamietcatto@gmail.com>
2023-02-08 17:56:31 +0100

    Removed logo background to improve the appearance.

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import Search from './Search';
 import SearchResults from './SearchResults';
 import '../styles/app.css';
+import logoNoBg from '../images/nasa-worm-logo-no-bg.png';
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
 
   return (
     <div className="app">
-      <img src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" 
-      alt="NASA Logo" className="nasa-logo"></img>
+      <img src={logoNoBg}
+      alt="NASA Logo" 
+      className="nasa-logo"></img>
       <Search
       setSearchResults={setSearchResults} />
       <SearchResults results={searchResults}/>
